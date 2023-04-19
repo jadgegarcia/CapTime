c32c050b2202f11918b5de55f90d75d402242a5b
JudgeMe-2 <120168235+JudgeMe-2@users.noreply.github.com>
2023-04-19 11:22:31 +0800

    Minor changes

--- app/src/main/java/com/example/captime/SignUp.java
+++ app/src/main/java/com/example/captime/SignUp.java
@@ -7,8 +7,11 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class SignUp extends AppCompatActivity {
-    Button already_account;
+    Button already_account, register_button;
+    TextInputLayout full_name, username, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +19,79 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         already_account = findViewById(R.id.btn_already_acc);
-
+        register_button = findViewById(R.id.btn_reg);
+        full_name = findViewById(R.id.et_fullname);
+        username = findViewById(R.id.et_username);
+        password = findViewById(R.id.et_password);
         already_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
+    }
+    // for email "[a-zA-z0-9._-]+@[a-z]+\\.+[a-z]+"
+    private Boolean validateName() {
+        String val = full_name.getEditText().getText().toString();
+        if(val.isEmpty()) {
+            full_name.setError("Field cannot be empty");
+            return false;
+        } else {
+            full_name.setError(null);
+            full_name.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private Boolean validateUserName() {
+        String val = username.getEditText().getText().toString();
+        String noWhiteSpace = "\\A\\w{4,20}\\z";
+        if(val.isBlank()) {
+            username.setError("Field cannot be empty");
+            return false;
+        }else if(val.length() >= 15) {
+            username.setError("Username too long");
+            return false;
+        }else if(!val.matches(noWhiteSpace)) {
+            username.setError("White Spaces not allowed");
+            return false;
+        }else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private Boolean validatePassword() {
+        String val = password.getEditText().getText().toString();
+        String passwordVal = "^" +
+                //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                "(?=.*[@#$%^&+=.])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+        if (val.isEmpty()) {
+            password.setError("Field cannot be empty");
+            return false;
+        } else if (!val.matches(passwordVal)) {
+            password.setError("Password must have atleast 1 character, no white spaces and atleast 4 characters");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+    public void registerUser(View view) {
+
+        if(!validateName() | !validateUserName() | !validatePassword()) {
+            return;
+        }
+
+        //Get user information input
+        String name = full_name.getEditText().getText().toString();
+        String uname = username.getEditText().getText().toString();
+        String pass = password.getEditText().getText().toString();
     }
 }
