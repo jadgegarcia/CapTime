e8b631543ff814674b23f47be3c1b0c0537cd34e
JudgeMe-2 <120168235+JudgeMe-2@users.noreply.github.com>
2023-04-21 14:39:29 +0800

    Fixed Splash screen, Login and Sign up bugs

--- app/src/main/java/com/example/captime/SignUp.java
+++ app/src/main/java/com/example/captime/SignUp.java
@@ -2,6 +2,7 @@ package com.example.captime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -26,6 +27,8 @@ public class SignUp extends AppCompatActivity {
         already_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(SignUp.this, Login.class);
+                startActivity(intent);
                 finish();
             }
         });
